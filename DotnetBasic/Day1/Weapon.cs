3af16c4ccace87e0a9b1923a143b3a6dfcc33d43
vncapit <vncapit@gmail.com>
2025-01-01 20:12:36 -0800

    First day, basic class + property

---
+++ DotnetBasic/Day1/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBasic.Day1
+{
+    public enum WeaponType
+    {
+        Handgun,
+        Shotgun,
+        Rifle
+    }
+    internal class Weapon(string name, WeaponType weaponType)
+    {
+        public string Name { get; } = name;
+        WeaponType weaponType;
+        public WeaponType WeaponType { get; } = weaponType;
+        int damage;
+        public int Damage {
+            get { return this.damage; }
+            set
+            {
+                if (this.damage <= 0) throw new Exception("Bad damage");
+                this.damage = value;
+            }
+        }
+
+        public int Weight {  get; set; }
+
+        public override string ToString() {
+            return $"This is a {WeaponType}";
+        }
+    }
+}
---
+++ DotnetBasic/Program.cs
@@ -0,0 +1,24 @@
+﻿using DotnetBasic.Day1;
+
+namespace DotnetBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Weapon ak47 = new Weapon("AK47", WeaponType.Rifle);
+                ak47.Weight = 5200;
+                ak47.Damage = 100;
+                Console.WriteLine(ak47.ToString());
+
+            }
+            catch (Exception ex) { 
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
