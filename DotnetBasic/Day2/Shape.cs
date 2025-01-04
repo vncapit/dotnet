04726394a6302060bf1766a4e00c3503d42a3315
vncapit <vncapit@gmail.com>
2025-01-04 01:46:04 -0800

    Abstract Class/Interface

---
+++ DotnetBasic/Day2/Shape.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBasic.Day2
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+        public virtual void Info()
+        {
+            Console.WriteLine($"This is a {Name}");
+        }
+
+        public abstract double CalPerimeter();
+
+        public abstract double CalArea();
+    }
+
+    class Circle : Shape { 
+    
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Name = "Circle";
+            Radius = radius;
+        }
+
+        public override double CalPerimeter()
+        {
+            return 2 * Radius * Math.PI;
+        }
+
+        public override double CalArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine($"This is a circle with radius equal to {Radius}");
+        }
+
+        public void CircleSpecialMethod() {
+            Console.WriteLine("Method only in Circle");
+        }
+    }
+
+    interface IShape
+    {
+        string Name { get; set; }
+        void Info();
+        double CalPerimeter();
+        double CalArea();
+    }
+
+    public class Rectangle : IShape
+    {
+        public string Name { get; set; }
+
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Name = "Rectangle";
+            Width = width;
+            Height = height;
+        }
+
+        public double CalArea()
+        {
+            return Width * Height;
+        }
+
+        public double CalPerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+
+        public void Info()
+        {
+            Console.WriteLine($"This is a rectangle with size is [{Width}, {Height}]");
+        }
+    }
+}
--- DotnetBasic/Program.cs
+++ DotnetBasic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DotnetBasic.Day1;
+using DotnetBasic.Day2;
 
 namespace DotnetBasic
 {
@@ -8,10 +9,22 @@ namespace DotnetBasic
         {
             try
             {
-                Weapon ak47 = new Weapon("AK47", WeaponType.Rifle);
-                ak47.Weight = 5200;
-                ak47.Damage = 100;
-                Console.WriteLine(ak47.ToString());
+                //Weapon ak47 = new Weapon("AK47", WeaponType.Rifle);
+                //ak47.Weight = 5200;
+                //ak47.Damage = 100;
+                //Console.WriteLine(ak47.ToString());
+
+                Shape circle = new Circle(10);
+                circle.Info();
+
+                (circle as Circle).CircleSpecialMethod();
+
+                
+                Circle newCircle = new Circle(10);
+                newCircle.CircleSpecialMethod();
+
+                IShape rectangle = new Rectangle(10, 5);
+                rectangle.Info();
 
             }
             catch (Exception ex) { 
