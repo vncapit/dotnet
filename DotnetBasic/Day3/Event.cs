c9210d55aaf39c0daac520a966b166069488d3b0
vncapit <vncapit@gmail.com>
2025-01-06 03:08:09 -0800

    Delegate and Event

---
+++ DotnetBasic/Day3/Event.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetBasic.Day3
+{
+    internal class UserInput
+    {
+
+        public delegate void DoMath(int input);
+
+        public DoMath? doMath;
+
+        public event EventHandler onUserInputed;
+        
+        public void Run()
+        {
+            do
+            {
+                Console.WriteLine("Please input a number: ");
+                var inputString = Console.ReadLine();
+                if (string.IsNullOrEmpty(inputString)) continue;
+                var inputNumber = Int32.Parse(inputString);
+                doMath?.Invoke(inputNumber);
+                onUserInputed?.Invoke(this, new UserInputEventArgs(inputNumber));
+            }
+            while (true);
+
+        }
+    }
+
+    class SubcriberClass1
+    {
+        public void PrintAddOne(int value)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{value} + 1 = {value + 1}");
+            Console.ResetColor();
+        }
+
+        public void PrintAddOneHandler(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            int value = (e as UserInputEventArgs).Input;
+            Console.WriteLine($"From event handler: {value} + 1 = {value + 1}");
+            Console.ResetColor();
+        }
+    }
+
+    class SubcriberClass2
+    {
+        public void PrintMinosOne(int value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{value} - 1 = {value - 1}");
+            Console.ResetColor();
+        }
+
+        public void PrintMinosOneHandler(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int value = (e as UserInputEventArgs).Input;
+            Console.WriteLine($"From event handler: {value} - 1 = {value - 1}");
+            Console.ResetColor();
+        }
+    }
+
+    class UserInputEventArgs : EventArgs {
+        public int Input {  get; set; }
+        public UserInputEventArgs(int input) { 
+            this.Input = input;
+        }
+    }
+
+
+}
--- DotnetBasic/Program.cs
+++ DotnetBasic/Program.cs
@@ -1,76 +1,25 @@
 ﻿using System.Diagnostics;
 using DotnetBasic.Day1;
 using DotnetBasic.Day2;
+using DotnetBasic.Day3;
 
 namespace DotnetBasic
 {
     internal class Program
     {
-        delegate void Print(string text);
-        static void PrintInfo(string text)
-        {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.WriteLine(text);
-            Console.ResetColor();
-        }
-        static void PrintWarning(string text)
-        {
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.WriteLine(text);
-            Console.ResetColor();
-        }
-
-        static void WriteLog(string text, Print print)
-        {
-            print(text);
-        }
-
-        static Action<string> printAction;
-
-        static Func<int, int, int> mathFucn;
-
-        static int findMax(int a, int b) => a > b ? a : b;
-
         static void Main(string[] args)
         {
             try
             {
-                Print print = null;
-                print += PrintInfo;
-                //print("Abc");
-                print += PrintWarning;
-                //print("xyz");
-                print.Invoke("ZZZ");
+                UserInput userInput = new UserInput();
+                SubcriberClass1 sub1 = new SubcriberClass1();
+                SubcriberClass2 sub2 = new SubcriberClass2();
+                userInput.doMath += sub1.PrintAddOne;
+                userInput.doMath += sub2.PrintMinosOne;
 
-                WriteLog("wwwwwwwwwww", PrintWarning);
-
-                printAction?.Invoke("ddd");
-                printAction += PrintInfo;
-                printAction.Invoke("Print info");
-
-                mathFucn += findMax;
-                int a = 3;
-                int b = 5;
-                Console.WriteLine($"Max of {a} and {b} is {mathFucn.Invoke(a, b)}");
-
-
-                //Weapon ak47 = new Weapon("AK47", WeaponType.Rifle);
-                //ak47.Weight = 5200;
-                //ak47.Damage = 100;
-                //Console.WriteLine(ak47.ToString());
-
-                //Shape circle = new Circle(10);
-                //circle.Info();
-
-                //(circle as Circle).CircleSpecialMethod();
-
-                
-                //Circle newCircle = new Circle(10);
-                //newCircle.CircleSpecialMethod();
-
-                //IShape rectangle = new Rectangle(10, 5);
-                //rectangle.Info();
-
+                userInput.onUserInputed += sub1.PrintAddOneHandler;
+                userInput.onUserInputed += sub2.PrintMinosOneHandler;
+                userInput.Run();
 
             }
             catch (Exception ex) { 
